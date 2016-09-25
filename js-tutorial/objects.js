6f89e0d50ebc3502f0effd3a4247820b7c18a269
egor <maiorov.prog@gmail.com>
2016-09-25 23:16:43 +0300

    finished object intro

--- js-tutorial/objects.js
+++ js-tutorial/objects.js
@@ -2,7 +2,23 @@ var person = {};
 
 person.firstName = 'Andrew'
 person.lastName = 'Mead'
+person.age = 23
+
+delete person.age
 
 function greetUser(person) {
   console.log('Hello ' + person.firstName + ' ' + person.lastName)
 }
+
+greetUser(person)
+
+var myCat = {
+  type:'cat',
+  'name': 'Charles'
+}
+
+function printPet(pet) {
+  console.log('You have a ' + pet.type + ' named ' + pet.name )
+}
+
+printPet(myCat)
