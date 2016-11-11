f93805d4fc4c37f235b5df28cf701eb7efc0a65a
egor <maiorov.prog@gmail.com>
2016-11-11 21:54:50 +0200

    added serving sttic abilitity to hello-epxress app

--- hello-express/server.js
+++ hello-express/server.js
@@ -1,6 +1,6 @@
 var express = require('express')
 var app = express()
-
+var PORT = 3000
 
 app.get("/", function(req,res) {
   res.send('Hello Express')
@@ -10,4 +10,9 @@ app.get("/about", function(req, res){
   res.send('<h1>About us</h1>')
 })
 
-app.listen(3000)
+app.use(express.static(__dirname+"/public"));
+console.log(__dirname+"/public")
+
+app.listen(PORT, function() {
+  console.log('Express Server is started on port ' + PORT)
+})
