bc6b1147e24cac99cd3796519838007cf090f5f3
egor <maiorov.prog@gmail.com>
2016-11-11 22:12:44 +0200

    added some middleware to express server

--- hello-express/server.js
+++ hello-express/server.js
@@ -2,11 +2,23 @@ var express = require('express')
 var app = express()
 var PORT = 3000
 
-app.get("/", function(req,res) {
-  res.send('Hello Express')
-})
+var middleware = {
+    requireAuthentication : function(req, res, next) {
+      console.log('private route hit!')
+      next()
+    },
+    logger: function(req,res,next) {
+      console.log('Request:' + new Date().toString() + ' ' + req.method + ' ' + req.originalUrl)
+      next()
+    }
+}
 
-app.get("/about", function(req, res){
+app.use(middleware.logger)
+
+// app.use(middleware.requireAuthentication);
+
+// we can add route level middleware
+app.get("/about", middleware.requireAuthentication,function(req, res){
   res.send('<h1>About us</h1>')
 })
 
