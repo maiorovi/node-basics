d0cae83567c57fdaa54d56d7374010a6a30c0e22
egor <maiorov.prog@gmail.com>
2016-11-13 18:58:06 +0200

    fixed problem with the id

--- rest-api/server.js
+++ rest-api/server.js
@@ -1,9 +1,37 @@
  var express = require('express')
  var app  = express();
  var PORT = process.env.PORT || 3000
+ var todos = [
+   {id:1, description: 'Meet mom for lunch', completed: false},
+   {id:2, description: 'Go to market', completed: false},
+   {id:3, description: 'Feed the cat', completed: true}
+ ]
 
  app.get('/', function(req,res){
    res.send('TODO Root Api')
+ })
+
+ // GET /todos
+ app.get('/todos', function(req, res) {
+   res.json(todos)
+ })
+
+ // GET /todos/id
+ app.get('/todos/:id', function(req, res) {
+   var matchedTodo;
+
+   todos.forEach(function(todo) {
+     if (todo.id == req.params.id) {
+       matchedTodo = todo
+     }
+   })
+
+   if(matchedTodo) {
+     res.json(matchedTodo)
+   } else {
+     res.status(404).send('Not found')
+   }
+
  })
 
  app.listen(PORT, function() {
