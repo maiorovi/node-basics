6c6fca9672dab91f3c1b115b535107625338ec02
egor <maiorov.prog@gmail.com>
2016-11-18 19:47:13 +0200

    hello yargs

--- async-basic/app.js
+++ async-basic/app.js
@@ -2,21 +2,28 @@
 // appid=f738464ba8ca5bbf0c3c78df3766706e
 // http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=f738464ba8ca5bbf0c3c78df3766706e
 
-var request = require('request')
+var args = require('yargs').argv
 var weather = require('./weather.js')
 var location = require('./location.js')
-
-weather(function(msg) {
+console.log(args.l)
+if (args.l) {
+weather(args.l, function(msg) {
   console.log(msg)
 })
+} else {
+  location(function(location){
+    if (!location) {
+      console.log('Location doesnt exist')
+      return
+    }
+    console.log('City:' + location.city)
+    console.log('log/lat:' + location.loc )
 
-location(function(location){
-  if (!location) {
-    console.log('Location doesnt exist')
-    return
-  }
-  console.log('City:' + location.city)
-  console.log('log/lat:' + location.loc )
-})
+    weather(location, function(msg) {
+      console.log(msg)
+    })
+  })
 
-console.log("After Request!")
+  console.log("After Request!")
+
+}
